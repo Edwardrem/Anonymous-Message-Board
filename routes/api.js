c0a49962cd694f254c555edf5536a49faaa4c376
Glitch (pale-toothbrush) <none>
2019-04-19 02:47:12 +0000

    🎃🐉 Checkpoint ./routes/api.js:3298705/672

--- routes/api.js
+++ routes/api.js
@@ -55,10 +55,10 @@ I can GET an array of the most recent 10 bumped threads on the board with only t
 I can POST a thread to a specific message board by passing form data text and delete_password to /api/threads/{board}.
 (Recomend res.redirect to board page /b/{board}) Saved will be _id, text, created_on(date&time), bumped_on(date&time, starts same as created_on), reported(boolean), delete_password, & replies(array).
 
-  // route should res.redirect to board page `/b/${board}`
   POST '/api/threads/:board'
+  // route should res.redirect to board page `/b/${board}`
   // .send( { text, delete_password })
-  thread document will contain:
+  // thread document will contain:
    {
       _id: // could be the native document_id,
       text: { type: String },
@@ -72,8 +72,18 @@ I can POST a thread to a specific message board by passing form data text and de
     
 I can report a thread and change it's reported value to true by sending a PUT request to /api/threads/{board} and pass along the thread_id. (Text response will be 'success') 
 
+  PUT '/api/threads/:board'
+  // .send({ thread_id })
+  // thread's reported field will be updated to true
+  // res.status(200).send('success');
+
 
 I can delete a thread completely if I send a DELETE request to /api/threads/{board} and pass along the thread_id & delete_password. (Text response will be 'incorrect password' or 'success')
+
+DELETE '/api/threads/:board'
+// .send({ thread_id, delete_password })
+// remove this thread from ${baord}'s database of threads
+// res.status(400).send('incorrect password'); || res.status(200).send('success')
 
 
 I can GET an entire thread with all it's replies from /api/replies/{board}?thread_id={thread_id}. Also hiding the same fields.
