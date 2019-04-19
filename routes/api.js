dab0b3375022c80543ee27900b2a65e7cb4d500f
Glitch (pale-toothbrush) <none>
2019-04-19 01:39:20 +0000

    🔨🌅 Checkpoint ./routes/api.js:3298705/46

--- routes/api.js
+++ routes/api.js
@@ -38,6 +38,10 @@ module.exports = app => {
 
 /*
 
+'/api/threads/:board'
+'/api/replies/:board'
+
+
 I can POST a thread to a specific message board by passing form data text and delete_password to /api/threads/{board}.
 (Recomend res.redirect to board page /b/{board}) Saved will be _id, text, created_on(date&time), bumped_on(date&time, starts same as created_on), reported(boolean), delete_password, & replies(array).
 
