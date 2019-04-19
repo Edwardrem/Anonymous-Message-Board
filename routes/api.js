db6cb9e98e9e3627de6aebbf87c5f3efaf52f707
Glitch (pale-toothbrush) <none>
2019-04-19 02:58:12 +0000

    🎎👹 Checkpoint ./routes/api.js:3298705/578

--- routes/api.js
+++ routes/api.js
@@ -58,18 +58,18 @@ I can POST a thread to a specific message board by passing form data text and de
   POST '/api/threads/:board'
   // route should res.redirect to board page `/b/${board}`
   // .send( { text, delete_password })
-  // thread document will contain:
-   {
-      _id: // could be the native document_id,
-      text: { type: String },
-      created_on(date&time): { type: Date },
-      bumped_on(date&time initializes to equal created_on: { type: Date },
-      reported: { type: Boolean },
-      delete_password: { type: String },
-      replies: { type: Array }
-    }
+  // thread document will contain: {
+        _id: // could be the native document_id,
+        text: { type: String },
+        created_on(date&time): { type: Date },
+        bumped_on(date&time initializes to equal created_on: { type: Date },
+        reported: { type: Boolean },
+        delete_password: { type: String },
+        replies: { type: Array } 
+      }
     
-    
+   
+   
 I can report a thread and change it's reported value to true by sending a PUT request to /api/threads/{board} and pass along the thread_id. (Text response will be 'success') 
 
   PUT '/api/threads/:board'
@@ -78,24 +78,34 @@ I can report a thread and change it's reported value to true by sending a PUT re
   // res.status(200).send('success');
 
 
+
 I can delete a thread completely if I send a DELETE request to /api/threads/{board} and pass along the thread_id & delete_password. (Text response will be 'incorrect password' or 'success')
 
-DELETE '/api/threads/:board'
-// .send({ thread_id, delete_password })
-// remove this thread from ${baord}'s database of threads
-// res.status(400).send('incorrect password'); || res.status(200).send('success')
+  DELETE '/api/threads/:board'
+  // .send({ thread_id, delete_password })
+  // remove this thread from ${baord}'s database of threads
+  // res.status(400).send('incorrect password'); || res.status(200).send('success')
+
 
 
 I can GET an entire thread with all it's replies from /api/replies/{board}?thread_id={thread_id}. Also hiding the same fields.
 
+  GET '/api/replies/:board'
+  // .query({ thread_id })
+  // return ALL replies from corresponding thread
+  // omit the fields: '-delete_password, -reported'
+
+
 
 I can POST a reply to a thead on a specific board by passing form data text, delete_password, & thread_id to /api/replies/{board} and it will also update the bumped_on date to the comments date.(Recomend res.redirect to thread page /b/{board}/{thread_id}) In the thread's 'replies' array will be saved _id, text, created_on, delete_password, & reported.
 
-// route should res.redirect to board page `/b/${board}`
 POST '/api/replies/:board'
+// route should res.redirect to board page `/b/${board}`
+
 
 
 I can report a reply and change it's reported value to true by sending a PUT request to /api/replies/{board} and pass along the thread_id & reply_id. (Text response will be 'success')
+
 
 
 I can delete a post(just changing the text to '[deleted]') if I send a DELETE request to /api/replies/{board} and pass along the thread_id, reply_id, & delete_password. (Text response will be 'incorrect password' or 'success')
