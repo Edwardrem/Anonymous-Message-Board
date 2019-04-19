abe7955b0857b00d8741cfbd0dfa5c46f2caff27
Glitch (pale-toothbrush) <none>
2019-04-19 04:18:17 +0000

    👧🚿 Checkpoint ./routes/api.js:3298705/231

--- routes/api.js
+++ routes/api.js
@@ -129,6 +129,8 @@ I can report a reply and change it's reported value to true by sending a PUT req
 I can delete a post(just changing the text to '[deleted]') if I send a DELETE request to /api/replies/{board} and pass along the thread_id, reply_id, & delete_password. (Text response will be 'incorrect password' or 'success')
 
 DELETE '/api/replies/:board'
-// 
+// .send({ thread_id, reply_id, delete_password })
+// the reply's text field will be updated to equal: [deleted]
+// res.status(400).send('incorrect password'); || res.status(200).send('success')
 
 */
