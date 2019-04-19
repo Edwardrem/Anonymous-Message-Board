4092d1dd84eecc7ded3f2782a170e40235a25b71
Glitch (pale-toothbrush) <none>
2019-04-19 06:08:31 +0000

    🚊🏮 Checkpoint ./models/Thread.js:3298705/522

--- models/Thread.js
+++ models/Thread.js
@@ -3,18 +3,16 @@ const Schema = mongoose.Schema;
 
 const ReplySchema = new Schema({
   text: { type: String },
-  created_on: { type: Date }
+  created_on: { type: Date },
+  delete_password: { type: String },
+  reported: { type: Boolean, default: false }
 }, { _id: true });
+
+const ThreadSchema = new Schema()
 
 /*
 
 Example Schema:
-
-const ExerciseSchema = new Schema({
-  description: { type: String, required: true },
-  duration: { type: Number, min: 1, required: true },
-  date: { type: String }
-}, { _id: false });
 
 const UserSchema = new Schema({
   _id: { type: String, required: true },
@@ -37,15 +35,6 @@ thread document will contain:
     reported: { type: Boolean },
     delete_password: { type: String },
     replies: { type: Array } 
-  }
- 
-replies Array consists of:
-  {
-    _id: new ObjectId();,
-    text: { type: String },
-    created_on: { type: Date },
-    delete_password: { type: String },
-    reported: { type: Boolean, default: false }
   }
 
 */
