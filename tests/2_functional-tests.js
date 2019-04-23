285dcc17f8cf3f43d356c57b655d12a966d8e463
Glitch (pale-toothbrush) <none>
2019-04-23 04:02:27 +0000

    🌜📕 Checkpoint ./tests/2_functional-tests.js:3298705/696

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -16,18 +16,22 @@ chai.use(chaiHttp);
 suite('Functional Tests', () => {
   suite('API ROUTING FOR /api/threads/:board', () => {
     suite('POST', () => {
-      test('Redirect after created a board', done => {
+      test('Redirect after creating a board', done => {
         chai.request(server).post('/api/threads/general/').send({
           text: 'test',
           delete_password: 'password'
         }).end((err, res) => {
-          console.log(res);
+          assert.equal(res.status, 200);
+          assert.equal(res.redirects[0].slice(-11), '/b/general/');
+          done();
         });
       });
     });
     
-    suite.skip('GET', () => {
-      
+    suite('GET', () => {
+      test.skip('GET 10 bumped threads with 3 replies', done => {
+        chai.request(server).get('/api/threads/general/')
+      });
     });
     
     suite.skip('DELETE', () => {
